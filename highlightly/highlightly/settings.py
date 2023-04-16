d76760c78d7598ba967a6948848004b223d4597a
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-16 21:10:14 +0200

    Added django cors headers

--- highlightly/highlightly/settings.py
+++ highlightly/highlightly/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'knox',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
