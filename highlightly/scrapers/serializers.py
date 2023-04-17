3b0bdb1e08b2a3fc88f67f4c88b0d9848952eeba
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 22:01:13 +0200

    Added team serializer

--- highlightly/scrapers/serializers.py
+++ highlightly/scrapers/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from scrapers.models import ScheduledMatch, Tournament
+from scrapers.models import Tournament, Team
 from util.file_util import get_base64
 
 
@@ -10,6 +10,18 @@ class TournamentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tournament
         fields = ["id", "game", "name", "logo", "url"]
+
+    @staticmethod
+    def get_logo(tournament: Tournament) -> str:
+        return get_base64(tournament.logo_filename)
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Team
+        fields = ["id", "game", "name", "logo", "nationality", "ranking", "url"]
 
     @staticmethod
     def get_logo(tournament: Tournament) -> str:
