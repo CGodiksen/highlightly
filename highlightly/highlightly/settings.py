2531de3a35625b4b0e10a2eacfe14b8c61427dec
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 12:42:47 +0200

    Added application for extracting highlights

--- highlightly/highlightly/settings.py
+++ highlightly/highlightly/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'scrapers.apps.ScrapersConfig',
+    'highlights.apps.HighlightsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
