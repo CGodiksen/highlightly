993f6865c5fa66d4a08f6a3e43433f80498db1a7
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 23:33:24 +0200

    Fixed task structure and removed unnecessary prints

--- highlightly/scrapers/tasks.py
+++ highlightly/scrapers/tasks.py
@@ -4,25 +4,19 @@ from scrapers.scrapers.league_of_legends import LeagueOfLegendsScraper
 from scrapers.scrapers.valorant import ValorantScraper
 
 
-@app.task(bind=True)
-def scrape_counter_strike_matches(self) -> None:
+@app.task
+def scrape_counter_strike_matches() -> None:
     scraper = CounterStrikeScraper()
     scraper.scrape()
 
-    print(f"Request: {self.request!r}")
 
-
-@app.task(bind=True)
-def scrape_valorant_matches(self) -> None:
+@app.task
+def scrape_valorant_matches() -> None:
     scraper = ValorantScraper()
     scraper.scrape()
 
-    print(f"Request: {self.request!r}")
 
-
-@app.task(bind=True)
-def scrape_league_of_legends_matches(self) -> None:
+@app.task
+def scrape_league_of_legends_matches() -> None:
     scraper = LeagueOfLegendsScraper()
     scraper.scrape()
-
-    print(f"Request: {self.request!r}")
