422a938ce5784fe9b21041b85249b3bbddb41387
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 12:09:35 +0200

    Added tournament model

--- highlightly/scrapers/models.py
+++ highlightly/scrapers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.TextChoices):
+    COUNTER_STRIKE = "COUNTER_STRIKE", "Counter-Strike"
+    LEAGUE_OF_LEGENDS = "LEAGUE_OF_LEGENDS", "League of Legends"
+    VALORANT = "VALORANT", "Valorant"
+
+
+class Tournament(models.Model):
+    game = models.CharField(max_length=32, choices=Game.choices)
+    name = models.CharField(max_length=128)
+
+    logo_filename = models.CharField(max_length=256)
+    url = models.URLField(max_length=128)
