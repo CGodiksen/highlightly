3d07ace566adaceecf6beee9c78c4f9c45e67fce
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 12:33:50 +0200

    Added model for scheduled matches

--- highlightly/scrapers/models.py
+++ highlightly/scrapers/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -23,3 +23,25 @@ class Team(models.Model):
 
     nationality = models.CharField(max_length=256)
     ranking = models.IntegerField(validators=[MinValueValidator(1)])
+
+
+class ScheduledMatch(models.Model):
+    class Type(models.TextChoices):
+        BEST_OF_1 = "BEST_OF_1", "Best of 1"
+        BEST_OF_3 = "BEST_OF_3", "Best of 3"
+        BEST_OF_5 = "BEST_OF_5", "Best of 5"
+
+    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="scheduled_team_1_matches")
+    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="scheduled_team_2_matches")
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+    tournament_context = models.CharField(max_length=64)
+    type = models.CharField(max_length=16, choices=Type.choices)
+    tier = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    url = models.URLField(max_length=128)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField()
+    estimated_end_time = models.DateTimeField()
+
+    finished = models.BooleanField(default=False)
