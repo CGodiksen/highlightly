a3ec40d111649d876f7fc480ef8fe44a6242f3bf
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-17 22:03:46 +0200

    Added scheduled match serializer

--- highlightly/scrapers/serializers.py
+++ highlightly/scrapers/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from scrapers.models import Tournament, Team
+from scrapers.models import Tournament, Team, ScheduledMatch
 from util.file_util import get_base64
 
 
@@ -26,3 +26,14 @@ class TeamSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_logo(tournament: Tournament) -> str:
         return get_base64(tournament.logo_filename)
+
+
+class ScheduledMatchSerializer(serializers.ModelSerializer):
+    team_1 = TeamSerializer()
+    team_2 = TeamSerializer()
+    tournament = TournamentSerializer()
+
+    class Meta:
+        model = ScheduledMatch
+        fields = ["id", "team_1", "team_2", "tournament", "tournament_context", "type", "tier", "url", "created_at",
+                  "start_time", "estimated_end_time", "create_video", "finished"]
