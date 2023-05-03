92b53c3e44c38fd29cb1f005c3260b6f1a9d3999
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-03 21:40:20 +0200

    Fixed error with logo filename not updating

--- highlightly/scrapers/views.py
+++ highlightly/scrapers/views.py
@@ -52,23 +52,25 @@ class TeamViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.List
     def get_queryset(self) -> QuerySet[Team]:
         return Team.objects.all().order_by(F("ranking").asc(nulls_last=True))
 
-    def perform_update(self, serializer: ModelSerializer):
-        serializer.save()
+    def update(self, request: Request, *args, **kwargs) -> Response:
+        instance: Team = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
 
         # If a new logo is provided, replace the old image in local storage.
         if "logo_base64" in serializer.validated_data and serializer.validated_data["logo_base64"]:
-            team: Team = self.get_object()
-            old_filename = team.logo_filename
-
-            if old_filename != "default.png":
-                Path(f"media/teams/{old_filename}").unlink(missing_ok=True)
+            if instance.logo_filename != "default.png":
+                Path(f"media/teams/{instance.logo_filename}").unlink(missing_ok=True)
 
             new_filename = f"{serializer.validated_data['name'].replace(' ', '_')}.png"
             base64: str = serializer.validated_data["logo_base64"]
             save_base64_image(f"media/teams/{new_filename}", base64)
 
-            team.logo_filename = new_filename
-            team.save()
+            instance.logo_filename = new_filename
+            instance.save()
+
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
     @action(detail=True, methods=["POST"])
     def refresh_from_url(self, request: Request) -> Response:
