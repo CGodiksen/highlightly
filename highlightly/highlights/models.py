3d00217fbeae1ded4a9f1555f60d11ac4cb6c9a9
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-04 21:16:37 +0200

    Added value field to highlight model

--- highlightly/highlights/models.py
+++ highlightly/highlights/models.py
@@ -7,10 +7,11 @@ from scrapers.models import GameVod
 class Highlight(models.Model):
     game_vod = models.ForeignKey(GameVod, on_delete=models.CASCADE)
 
+    value = models.FloatField()
     start_time_seconds = models.IntegerField(validators=[MinValueValidator(0)])
     duration_seconds = models.IntegerField(validators=[MinValueValidator(5)])
     events = models.CharField(max_length=512)
     round_number = models.IntegerField(validators=[MinValueValidator(1)])
 
     def __str__(self) -> str:
-        return f"Round {self.round_number} ({self.duration_seconds}s): {self.events}"
+        return f"Round {self.round_number} ({self.value} - {self.duration_seconds}s): {self.events}"
