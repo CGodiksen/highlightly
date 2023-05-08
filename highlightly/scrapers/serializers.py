9e7fcd32a19ecdf8406ed899e7cbfd53870b4886
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-08 08:01:37 +0200

    Updated match serializer

--- highlightly/scrapers/serializers.py
+++ highlightly/scrapers/serializers.py
@@ -46,8 +46,8 @@ class MatchSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Match
-        fields = ["id", "team_1", "team_2", "tournament", "tournament_context", "type", "tier", "url", "created_at",
-                  "start_datetime", "estimated_end_datetime", "create_video", "finished"]
+        fields = ["id", "team_1", "team_2", "tournament", "tournament_context", "format", "tier", "url", "created_at",
+                  "start_datetime", "estimated_end_datetime", "create_video", "finished", "highlighted"]
 
 
 class MatchUpdateSerializer(serializers.ModelSerializer):
