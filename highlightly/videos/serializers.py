7531907d58fb592e07c12d6a66b18be5d21c5f65
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-10 14:48:27 +0200

    Added serializer for updating video metadata

--- highlightly/videos/serializers.py
+++ highlightly/videos/serializers.py
@@ -9,10 +9,17 @@ class VideoMetadataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = VideoMetadata
-        fields = ["id", "match", "title", "description", "tags", "thumbnail"]
+        fields = ["id", "match", "title", "description", "tags", "thumbnail", "language", "category_id"]
 
     @staticmethod
     def get_thumbnail(video_metadata: VideoMetadata) -> str:
         return get_base64(f"{video_metadata.match.create_unique_folder_path()}/{video_metadata.thumbnail_filename}")
 
 
+class VideoMetadataUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VideoMetadata
+        fields = ["title", "description", "tags", "language", "category_id"]
+
+    def to_representation(self, video_metadata: VideoMetadata) -> dict:
+        return VideoMetadataSerializer(video_metadata).data
