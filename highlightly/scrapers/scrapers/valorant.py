7e838750181101ecff23852c094535a575ddb79d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-15 23:26:11 +0200

    Only add the match if is from an included tournament and both teams are determined

--- highlightly/scrapers/scrapers/valorant.py
+++ highlightly/scrapers/scrapers/valorant.py
@@ -9,8 +9,11 @@ from scrapers.types import TeamData
 class ValorantScraper(Scraper):
     """Webscraper that scrapes vlr.gg for upcoming Valorant matches."""
 
-    @staticmethod
-    def list_upcoming_matches() -> list[dict]:
+    included_tournaments = ["Champions Tour 2023: Americas League", "Champions Tour 2023: EMEA League",
+                            "Champions Tour 2023: Pacific League", "Champions Tour 2023: Masters Tokyo",
+                            "Challengers League: North America"]
+
+    def list_upcoming_matches(self) -> list[dict]:
         html = requests.get(url="https://www.vlr.gg/matches").text
         soup = BeautifulSoup(html, "html.parser")
 
@@ -19,7 +22,12 @@ class ValorantScraper(Scraper):
 
         # For each match, extract the data necessary to create a tournament, teams, and match objects.
         for match_row in match_rows:
-            print(match_row.find("div", class_="match-item-vs-team-name").text.strip())
+            tournament_name = match_row.find("div", class_="match-item-event").text.split("\n")[-1].strip()
+            team_names = [team.text.strip() for team in match_row.findAll("div", class_="match-item-vs-team-name")]
+
+            # Only add the match if is from an included tournament and both teams are determined.
+            if tournament_name in self.included_tournaments and "TBD" not in team_names:
+                print(team_names)
 
         return []
 
