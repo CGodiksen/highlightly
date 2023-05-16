745ca61260128dd8e65423e92a02748cb7b5a27d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-16 16:31:55 +0200

    Added initial tuning of zoom for different games

--- highlightly/videos/metadata/util.py
+++ highlightly/videos/metadata/util.py
@@ -3,13 +3,19 @@ from PIL import Image
 from scrapers.models import Game
 
 
-# TODO: For League of Legends and valorant, zoom into the image center way more.
 def create_match_frame_part(match_frame_filepath: str, team_part_width: int, game: Game) -> Image.Image:
     """
     Given a filepath to a full size frame from a match, resize and crop the image to make it the correct size for the
     match frame part of the thumbnail.
     """
     match_frame = Image.open(match_frame_filepath).resize((1920, 1080))
+
+    # For League of Legends and valorant, zoom into the center of the image.
+    if game == Game.LEAGUE_OF_LEGENDS:
+        match_frame = zoom_at(match_frame, match_frame.width / 2, match_frame.height / 3.5, 1.1)
+    elif game == Game.VALORANT:
+        match_frame = zoom_at(match_frame, match_frame.width / 1.70, match_frame.height / 2.5, 1.1)
+
     match_frame.thumbnail(get_game_match_frame_size(game))
 
     cropped_width = match_frame.width - (1280 - team_part_width)
@@ -24,4 +30,14 @@ def get_game_match_frame_size(game: Game) -> tuple[int, int]:
     if game == Game.COUNTER_STRIKE:
         return 1450, 820
     else:
-        return 1650, 1020
+        return 1920, 1080
+
+
+def zoom_at(img, x, y, zoom):
+    """Zoom into the image at the given point and return a zoomed image that is upscaled to the original size."""
+    w, h = img.size
+    zoom2 = zoom * 2
+    img = img.crop((x - w / zoom2, y - h / zoom2,
+                    x + w / zoom2, y + h / zoom2))
+
+    return img.resize((w, h), Image.LANCZOS)
