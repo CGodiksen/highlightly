3ea69495f379046ac6a145daba05308b25edd34c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-18 23:04:11 +0200

    Using multiprocessing to save video frames

--- highlightly/highlights/highlighters/valorant.py
+++ highlightly/highlights/highlighters/valorant.py
@@ -1,6 +1,9 @@
 import logging
+import subprocess
+from multiprocessing import Pool
 
 import cv2
+from paddleocr import PaddleOCR
 
 from highlights.highlighters.highlighter import Highlighter
 from highlights.types import SecondData
@@ -37,7 +40,18 @@ def extract_round_timeline(game: GameVod) -> dict[int, SecondData]:
     """Parse through the VOD to find the round number, round timer, and spike events for each second of the game."""
     round_timeline = {}
     vod_filepath = f"{game.match.create_unique_folder_path('vods')}/{game.filename}"
-    frame_folder_path = game.match.create_unique_folder_path("frames")
+    folder_path = game.match.create_unique_folder_path("frames")
+    grouped_frames = get_grouped_frames(vod_filepath)
+
+    with Pool(len(grouped_frames)) as p:
+        print(p.starmap(save_video_frames, [(vod_filepath, group, folder_path) for group in grouped_frames]))
+
+    cmd = f"paddleocr --image_dir {folder_path} --use_angle_cls false --lang en --use_gpu false --enable_mkldnn true --use_mp true"
+    subprocess.run(cmd, shell=True)
+
+    return round_timeline
+
+
 def get_grouped_frames(vod_filepath: str) -> list[list[int]]:
     """Find 10-minute groups of frames that need to be extracted from the given vod filepath."""
     grouped_frames = []
@@ -57,9 +71,26 @@ def get_grouped_frames(vod_filepath: str) -> list[list[int]]:
     return grouped_frames
 
 
+def save_video_frames(vod_filepath: str, frame_group: list[int], folder_path: str) -> None:
+    """Parse through the VOD for the frames in the given group and save them to the folder path."""
+    for frame_second in frame_group:
+        video_capture = cv2.VideoCapture(vod_filepath)
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, 60 * frame_second)
+        _res, frame = video_capture.read()
+
+        if frame is not None:
+            cropped_frame = frame[0:85, 910:1010]
+            cv2.imwrite(f"{folder_path}/{frame_second}.jpg", scale_image(cropped_frame, 250))
+
+
 def scale_image(image: any, scale_percent) -> any:
     """Scale the given image while keeping the aspect ratio."""
     width = int(image.shape[1] * scale_percent / 100)
     height = int(image.shape[0] * scale_percent / 100)
 
     return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+
+
+def add_kill_events(game: GameVod, round_timeline: dict[int, SecondData]) -> None:
+    """Check for kill events for each second in the round timeline and add the new events to the data."""
+    pass
