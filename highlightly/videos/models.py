2aa43da8c1f061fedd95f38b24224abe8cc53ed5
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-28 22:18:52 +0200

    Added match frame time to video metadata model

--- highlightly/videos/models.py
+++ highlightly/videos/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from scrapers.models import Match
@@ -9,7 +10,9 @@ class VideoMetadata(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=5000)
     tags = models.JSONField()
+
     thumbnail_filename = models.CharField(max_length=256)
+    thumbnail_match_frame_time = models.IntegerField(validators=[MinValueValidator(1)], blank=True, null=True)
 
     language = models.CharField(max_length=256, default="english")
     category_id = models.IntegerField(default=20)
