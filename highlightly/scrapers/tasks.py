64bb08bf38174be7e0940fef7ae2dc7a47fb171f
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-05-29 08:23:10 +0200

    Removed celery tasks to scrape finished matches

--- highlightly/scrapers/tasks.py
+++ highlightly/scrapers/tasks.py
@@ -1,8 +1,5 @@
 import logging
 
-import requests
-from bs4 import BeautifulSoup
-
 from highlightly.celery import app
 from scrapers.models import Match, Game
 from scrapers.scrapers.counter_strike import CounterStrikeScraper
@@ -26,30 +23,6 @@ def scrape_valorant_matches() -> None:
 def scrape_league_of_legends_matches() -> None:
     scraper = LeagueOfLegendsScraper()
     scraper.scrape_upcoming_matches()
-
-
-@app.task
-def scrape_finished_counter_strike_match(scheduled_match_id: int) -> None:
-    scheduled_match = Match.objects.get(id=scheduled_match_id)
-
-    scraper = CounterStrikeScraper()
-    scraper.scrape_finished_match(scheduled_match)
-
-
-@app.task
-def scrape_finished_valorant_match(scheduled_match_id: int) -> None:
-    scheduled_match = Match.objects.get(id=scheduled_match_id)
-
-    scraper = ValorantScraper()
-    scraper.scrape_finished_match(scheduled_match)
-
-
-@app.task
-def scrape_finished_league_of_legends_match(scheduled_match_id: int) -> None:
-    scheduled_match = Match.objects.get(id=scheduled_match_id)
-
-    scraper = LeagueOfLegendsScraper()
-    scraper.scrape_finished_match(scheduled_match)
 
 
 @app.task
